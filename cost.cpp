4e4301ae235d77645f0465bdb37381ff7cf28643
samguns <wang.vai@gmail.com>
2018-07-29 21:00:15 +0800

    My second cost function

--- cost.cpp
+++ cost.cpp
@@ -18,3 +18,14 @@ float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float
 
   return cost;
 }
+
+float inefficiency_cost(int target_speed, int intended_lane, int final_lane, vector<int> lane_speeds) {
+  /*
+  Cost becomes higher for trajectories with intended lane and final lane that have traffic slower than target_speed.
+  */
+
+  int delta_speed = abs(2 * target_speed - lane_speeds[intended_lane] - lane_speeds[final_lane]);
+  float cost = 1 - (exp(-delta_speed));
+
+  return cost;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "cost.h"
 
 using namespace std;
@@ -27,8 +28,36 @@ static void verify_cost_function() {
 
 }
 
+static void verify_second_cost_function() {
+  //Target speed of our vehicle
+  int target_speed = 10;
+
+  //Lane speeds for each lane
+  vector<int> lane_speeds = {6, 7, 8, 9};
+
+  //Test cases used for grading - do not change.
+  float cost;
+  cout << "Costs for (intended_lane, final_lane):" << endl;
+  cout << "----------------------------------------------------------" << endl;
+  cost = inefficiency_cost(target_speed, 3, 3, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
+  cost = inefficiency_cost(target_speed, 2, 3, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
+  cost = inefficiency_cost(target_speed, 2, 2, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(2, 2)" << endl;
+  cost = inefficiency_cost(target_speed, 1, 2, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(1, 2)" << endl;
+  cost = inefficiency_cost(target_speed, 1, 1, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(1, 1)" << endl;
+  cost = inefficiency_cost(target_speed, 0, 1, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(0, 1)" << endl;
+  cost = inefficiency_cost(target_speed, 0, 0, lane_speeds);
+  cout << "The cost is " << cost << " for " << "(0, 0)" << endl;
+}
+
 int main() {
-  verify_cost_function();
+  // verify_cost_function();
+  verify_second_cost_function();
 
   return 0;
 }
